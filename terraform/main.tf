72801b670b4a0b26dd767da07afbd145cb745020
juannguyen0211 <root@JN7N-PC.localdomain>
2025-10-07 00:08:01 +0700

    file main.tf outputs.tf -- changes can pass to terraform plan command

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,15 +3,21 @@ provider "aws" {
   region  = var.region
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
 
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
+  name = "huan-tetris-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-southeast-1a", "ap-southeast-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = var.tags
 }
 
 module "eks" {
@@ -20,8 +26,8 @@ module "eks" {
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
-  subnet_ids      = data.aws_subnets.default.ids
-  vpc_id          = data.aws_vpc.default.id
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
 
   enable_irsa = true
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,6 +13,16 @@ output "cluster_version" {
   value       = module.eks.cluster_version
 }
 
+output "vpc_id" {
+  description = "VPC ID used for EKS"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
 output "node_group_role_arn" {
   description = "IAM role ARN for node group"
   value       = module.eks.eks_managed_node_groups["default"].iam_role_arn
